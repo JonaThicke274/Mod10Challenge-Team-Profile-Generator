fb2fc6b823eca066be52e42fa536740e9176f619
JonaThicke274 <lipatajonathan274@gmail.com>
2022-09-29 20:32:33 -0400

    created tests and constructor for Intern class

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require("../lib/Intern.js")
+
+test("creates intern object", () => {
+    const internTest = new Intern("Jonathan", 274, "lipatajonathan274@gmail.com", "George Mason University");
+
+    expect(internTest.school).toEqual("George Mason University");
+});
+
+test("gets intern's school", () => {
+    const internTest = new Intern("Jonathan", 274, "lipatajonathan274@gmail.com", "George Mason University");
+
+    expect(internTest.getSchool()).toEqual("George Mason University");
+});
+
+test("returns intern's role", () => {
+    const internTest = new Intern("Jonathan", 274, "lipatajonathan274@gmail.com", "George Mason University");
+
+    expect(internTest.getRole()).toEqual("Intern");
+});
