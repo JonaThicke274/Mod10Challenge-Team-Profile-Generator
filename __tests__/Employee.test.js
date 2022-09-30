3c37df07caf09d8f1f1892833b933b3d464f359c
JonaThicke274 <lipatajonathan274@gmail.com>
2022-09-29 20:09:24 -0400

    added comments to Employee.test and Employee; created tests and constructor for Manager class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,29 +1,29 @@
+// Calls Employee.js
 const Employee = require("../lib/Employee.js");
 
-// Tests creating Employee object
-
 test("creates an employee object", () => {
-    const employeeTest = new Employee("Jonathan", "274", "lipatajonathan274@gmail.com");
+    const employeeTest = new Employee("Jonathan", 274, "lipatajonathan274@gmail.com");
 
     expect(employeeTest.name).toEqual("Jonathan");
-    expect(employeeTest.id).toEqual("274");
+    expect(employeeTest.id).toEqual(274);
     expect(employeeTest.email).toEqual("lipatajonathan274@gmail.com");
 });
 
+
 test("gets employee's name", () => {
-    const employeeTest = new Employee("Jonathan", "274", "lipatajonathan274@gmail.com");
+    const employeeTest = new Employee("Jonathan", 274, "lipatajonathan274@gmail.com");
 
     expect(employeeTest.getName()).toEqual("Jonathan");
 });
 
 test("gets employee's id", () => {
-    const employeeTest = new Employee("Jonathan", "274", "lipatajonathan274@gmail.com");
+    const employeeTest = new Employee("Jonathan", 274, "lipatajonathan274@gmail.com");
 
-    expect(employeeTest.getId()).toEqual("274");
+    expect(employeeTest.getId()).toEqual(274);
 });
 
 test("gets employee's email", () => {
-    const employeeTest = new Employee("Jonathan", "274", "lipatajonathan274@gmail.com");
+    const employeeTest = new Employee("Jonathan", 274, "lipatajonathan274@gmail.com");
 
     expect(employeeTest.getEmail()).toEqual("lipatajonathan274@gmail.com");
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
+// Calls Manager.js
 const Manager = require("../lib/Manager.js");
+
+test("creates manager object", () => {
+    const managerTest = new Manager("Jonathan", 274, "lipatajonathan274@gmail.com", 420);
+
+    expect(managerTest.officeNumber).toEqual(420);
+});
+
+test("gets manager's role", () => {
+    const managerTest = new Manager("Jonathan", 274, "lipatajonathan274@gmail.com", 420);
+
+    expect(managerTest.getRole()).toEqual("Manager");
+});
